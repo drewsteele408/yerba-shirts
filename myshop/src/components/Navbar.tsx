df26eb00183208490ae12c705a9070fb4c08d199
Andrew Steele <104460430+drewsteele408@users.noreply.github.com>
2025-10-31 15:24:19 -0600

    home page and nav changes

---
+++ myshop/src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import {Link, NavLink} from "react-router-dom" 
+
+export default function NavBar() {
+    return (
+        <header className="navbar">
+            <nav className="navbar-container">
+                 {/* Brand / Logo */}
+            </nav>
+        </header>
+    )
+}
---
+++ myshop/src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+import {Link} from "react-router-dom"
+
+export default function HomePage() {
+    return (
+        <section>
+            <h1>Home</h1>
+            <p>Welcome to my Shop</p>
+        </section>
+    )
+}
